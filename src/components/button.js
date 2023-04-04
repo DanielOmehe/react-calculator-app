7e8997a36151f8c7ddcb9ac95d34b7414958d2a0
DaniOmehe <danielomehe66@gmail.com>
2023-04-04 17:15:58 +0100

    remove redundant code

--- src/components/button.js
+++ src/components/button.js
@@ -1,6 +1,5 @@
 import '../stylesheets/button.scss';
 const Button =({children, value, dispatch, type})=>{
-    console.log(value);
     return <button onClick={()=> dispatch({type: type, payload: { value }})}>{children}</button>
 }
 
